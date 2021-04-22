16c3a39bcfa1db4d2fed4fe0e19f7cae9166a0cb
John Jenkins <john.jenkins@nanoporetech.com>
2021-04-22 23:49:51 +0100

    Sourcemaps generated for collection, dist-custom-elements-bundle, dist-custom-elements, dist, esm, esm-es5 and cjs. Added option to StencilConfig type options.

--- src/compiler/transpile/run-program.ts
+++ src/compiler/transpile/run-program.ts
@@ -36,9 +36,9 @@ export const runTsProgram = async (
   const emitCallback: ts.WriteFileCallback = (emitFilePath, data, _w, _e, tsSourceFiles) => {
     const tsSourceFile = tsSourceFiles[0]?.fileName;
 
-    if (tsSourceFile.endsWith('.tsx') && tsSourceFile.match('/node_modules/') && emitFilePath.endsWith('.js')) return;
+    if (tsSourceFile.endsWith('.tsx') && tsSourceFile.match('/node_modules/') && (emitFilePath.endsWith('.js') || emitFilePath.endsWith('js.map'))) return;
 
-    if (emitFilePath.endsWith('.js')) {
+    if (emitFilePath.endsWith('.js') || emitFilePath.endsWith('js.map')) {
       updateModule(config, compilerCtx, buildCtx, tsSourceFiles[0], data, emitFilePath, tsTypeChecker, null);
 
       // re-reun transpile for any mixin dependents
