a99960a3062bda46e3bbd107bd45b9dcf2b4250b
johnjenkins <johnljenkins@hotmail.com>
2021-07-05 12:19:49 +0100

    fix(nested slots): non-shadow nesting slots without 'slot-fb' now works

--- src/declarations/stencil-private.ts
+++ src/declarations/stencil-private.ts
@@ -1561,6 +1561,11 @@ export interface RenderNode extends HostElement {
   ['s-sn']?: string;
 
   /**
+   * Parent slot name
+   */
+  ['s-psn']?: string;
+
+  /**
    * Is a slot fallback node
    */
   ['s-sf']?: boolean;
